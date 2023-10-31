9e1c381be51339f2884af86a032681e4bc56f9e8
Kevin Biju Kulangara <118618402+Kevin1077@users.noreply.github.com>
2023-10-31 14:13:31 +0530

    Add files via upload

---
+++ Sumofnumbers.java
@@ -0,0 +1,20 @@
+package sample;
+
+import java.util.Scanner;
+
+public class Sumofnumbers {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter two numbers");
+		
+		int number1=sc.nextInt();
+		int number2=sc.nextInt();
+		
+		int sum=number1+number2;
+		
+		System.out.println("Result is:"+sum);
+	}
+
+}
